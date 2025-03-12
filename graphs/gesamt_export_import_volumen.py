66889f4b7cac6adbc49c3a2df790ec12f447367d
maltemo1 <151633123+maltemo1@users.noreply.github.com>
2025-03-12 11:58:36 +0330

    Update gesamt_export_import_volumen.py

--- graphs/gesamt_export_import_volumen.py
+++ graphs/gesamt_export_import_volumen.py
@@ -1,51 +1,20 @@
 import dash
 from dash import dcc, html
-from dash.dependencies import Input, Output
 import pandas as pd
-import plotly.express as px
-from statistics import mean
-import glob
-from statsmodels.tsa.seasonal import seasonal_decompose
-import seaborn as sns
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.colors import to_rgba
-from matplotlib.ticker import FuncFormatter
-import dash_core_components as dcc
-import dash_html_components as html
-from dash.dependencies import Input, Output
 import plotly.graph_objects as go
-import math
-import gdown
-import os
+import numpy as np
 
-"""# Überblick über Deutschlands Handel
+def create_layout():
+    # CSV file path
+    data_file = 'data/1gesamt_deutschland.csv'
+    
+    # Read data
+    df_gesamt_deutschland = pd.read_csv(data_file)
 
-## Gesamtüberblick seit 2008 bis 2024
-
-### Gesamter Export-, Import- und Handelsvolumen-Verlauf Deutschlands
-"""
-
-# CSV-Datei einlesen
-df_gesamt_deutschland = pd.read_csv('data/1gesamt_deutschland.csv')
-
-# Dash-App erstellen
-app = dash.Dash(__name__)
-server = app.server
-
-app.layout = html.Div([
-    html.H1("Deutschlands Handelsentwicklung"),
-    dcc.Graph(id='handel_graph'),
-])
-
-@app.callback(
-    Output('handel_graph', 'figure'),
-    Input('handel_graph', 'id')
-)
-def update_graph(_):
+    # Create the graph
     fig = go.Figure()
 
-    # Linien für Export, Import und Handelsvolumen
+    # Add traces for Export, Import, and Trade Volume
     for col, name, color in zip(
         ['gesamt_export', 'gesamt_import', 'gesamt_handelsvolumen'],
         ['Exportvolumen', 'Importvolumen', 'Gesamthandelsvolumen'],
@@ -57,15 +26,15 @@ def update_graph(_):
             mode='lines+markers',
             name=name,
             line=dict(width=2, color=color),
-            hovertemplate=f'<b>{name}</b><br>Jahr: %{{x}}<br>Wert: %{{y:,.0f}} € €<extra></extra>'
+            hovertemplate=f'<b>{name}</b><br>Jahr: %{{x}}<br>Wert: %{{y:,.0f}} €<extra></extra>'
         ))
 
-    # Berechnung der maximalen Y-Achse für Tick-Werte
+    # Calculate Y-axis tick values
     max_value = df_gesamt_deutschland[['gesamt_export', 'gesamt_import', 'gesamt_handelsvolumen']].values.max()
-    tick_step = 500e9  # 500 Mrd als Schrittgröße
+    tick_step = 500e9  # 500 Mrd as step size
     tickvals = np.arange(0, max_value + tick_step, tick_step)
 
-    # Layout-Anpassungen
+    # Layout settings
     fig.update_layout(
         title='Entwicklung von Export, Import und Handelsvolumen',
         xaxis_title='Jahr',
@@ -78,7 +47,8 @@ def update_graph(_):
         legend=dict(title='Kategorie', bgcolor='rgba(255,255,255,0.7)')
     )
 
-    return fig
+    return html.Div([
+        html.H1("Deutschlands Handelsentwicklung"),
+        dcc.Graph(figure=fig)
+    ])
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
